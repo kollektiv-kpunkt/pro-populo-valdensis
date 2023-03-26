6eb42f04d6d80b00c269fbf3ebd6676bed7788ea
timothyoesch <54284649+timothyoesch@users.noreply.github.com>
2023-03-26 13:12:53 +0200

    Update Homepage

--- wordpress/wp-content/themes/pro-populo-valdensis/template-parts/blocks/content-toggle/layout.php
+++ wordpress/wp-content/themes/pro-populo-valdensis/template-parts/blocks/content-toggle/layout.php
@@ -1,5 +1,5 @@
 <div class="ppv-content-toggle-wrapper">
-    <details class="ppv-content-toggle mt-4 text-start"<?= (get_field("default_setting")) ? " open" : "" ?>>
+    <details class="ppv-content-toggle text-start"<?= (get_field("default_setting")) ? " open" : "" ?>>
         <summary class="ppv-content-toggle-summary flex justify-between leading-none">
             <span class="ppv-content-toggle-title"><?= get_field("title") ?></span>
             <div class="ppv-content-toggle-icon text-center"><i class="icofont-rounded-down text-2xl"></i></div>
